2381cda4cb47c55e52dae834d9b0ef261567f339
devesh140901 <deveshkumarsingh99@gmail.com>
2025-01-24 11:46:32 +0530

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create([
+    { name: 'Rakesh', preferred_working_hour_per_day: 7.5 },
+    { name: 'Umesh', preferred_working_hour_per_day: 7.5 }
+])

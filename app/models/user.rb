bb65b5852232fa6ed944c57d9bd72946c477af96
devesh140901 <deveshkumarsingh99@gmail.com>
2025-01-24 11:43:28 +0530

    added validations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :worklogs, dependent: :destroy
+
+    validates :name, presence: true
+    validates :preferred_working_hour_per_day, presence: true, numericality: { less_than_or_equal_to: 16 }
 end
--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -1,3 +1,6 @@
 class Worklog < ApplicationRecord
   belongs_to :user
+  validates :date, presence: true
+  validates :hours, presence: true, numericality: { greater_than: 0 }
+  validates :notes, presence: true
 end
